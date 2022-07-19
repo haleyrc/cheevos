e8d2904e8ff2d609920c58c8d72db41e8a19ab72
Ryan Haley <haley.rc@gmail.com>
2022-07-19 18:13:53 -0400

    Refactor validation into request method

--- user.go
+++ user.go
@@ -18,17 +18,25 @@ func (req *SignUpRequest) normalize() {
 	req.Password = strings.TrimSpace(req.Password)
 }
 
+func (req *SignUpRequest) validate() error {
+	if req.Username == "" {
+		return fmt.Errorf("invalid: username is blank")
+	}
+
+	if req.Password == "" {
+		return fmt.Errorf("invalid: password is blank")
+	}
+
+	return nil
+}
+
 type SignUpResponse struct{}
 
 func (us *UserService) SignUp(ctx context.Context, req SignUpRequest) (*SignUpResponse, error) {
 	req.normalize()
 
-	if req.Username == "" {
-		return nil, fmt.Errorf("invalid: username is blank")
-	}
-
-	if req.Password == "" {
-		return nil, fmt.Errorf("invalid: password is blank")
+	if err := req.validate(); err != nil {
+		return nil, fmt.Errorf("sign up failed: %w", err)
 	}
 
 	return nil, nil
