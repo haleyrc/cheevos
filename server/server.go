feb0e2b0688ab5a15b9a3a734fb3e50f2d554250
Ryan Haley <haley.rc@gmail.com>
2022-10-06 16:17:44 -0400

    WIP

--- server/server.go
+++ server/server.go
@@ -2,9 +2,12 @@ package server
 
 import (
 	"context"
+	"fmt"
+	"net/http"
 
 	"github.com/haleyrc/cheevos/auth"
 	"github.com/haleyrc/cheevos/cheevos"
+	"github.com/haleyrc/cheevos/lib/web"
 	"github.com/haleyrc/cheevos/roster"
 )
 
@@ -32,4 +35,38 @@ type RosterService interface {
 	DeclineInvitation(ctx context.Context, code string) error
 	InviteUserToOrganization(ctx context.Context, email, orgID string) (*roster.Invitation, error)
 	RefreshInvitation(ctx context.Context, invitationID string) (*roster.Invitation, error)
+}
+
+type Server struct {
+	Auth    AuthServer
+	Cheevos CheevosServer
+	Roster  RosterServer
+
+	mux *http.ServeMux
+}
+
+func (srv *Server) Start(ctx context.Context) error {
+	srv.registerRoutes()
+	if err := http.ListenAndServe(":8080", srv.mux); err != nil {
+		return fmt.Errorf("Server.Start: %w", err)
+	}
+	return nil
+}
+
+func (srv *Server) registerRoutes() {
+	if srv.mux == nil {
+		srv.mux = http.NewServeMux()
+	}
+
+	srv.mux.HandleFunc("/auth/SignUp", web.ResponseHandler(srv.Auth.SignUp))
+
+	srv.mux.HandleFunc("/cheevos/AwardCheevo", web.ResponseHandler(srv.Cheevos.AwardCheevo))
+	srv.mux.HandleFunc("/cheevos/CreateCheevo", web.ResponseHandler(srv.Cheevos.CreateCheevo))
+	srv.mux.HandleFunc("/cheevos/GetCheevo", web.ResponseHandler(srv.Cheevos.GetCheevo))
+
+	srv.mux.HandleFunc("/roster/AcceptInvitation", web.ResponseHandler(srv.Roster.AcceptInvitation))
+	srv.mux.HandleFunc("/roster/CreateOrganization", web.ResponseHandler(srv.Roster.CreateOrganization))
+	srv.mux.HandleFunc("/roster/DeclineInvitation", web.ResponseHandler(srv.Roster.DeclineInvitation))
+	srv.mux.HandleFunc("/roster/InviteUserToOrganization", web.ResponseHandler(srv.Roster.InviteUserToOrganization))
+	srv.mux.HandleFunc("/roster/RefreshInvitation", web.ResponseHandler(srv.Roster.RefreshInvitation))
 }
