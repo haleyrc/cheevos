cadb0ea45216af9bdd895fac49fd6d7e41644a71
Ryan Haley <haley.rc@gmail.com>
2022-12-07 21:15:18 -0500

    Unfreeze time after service tests

--- service/service_test.go
+++ service/service_test.go
@@ -11,6 +11,10 @@ import (
 func TestMain(m *testing.M) {
 	fmt.Println("=== FREEZING TIME ===")
 	time.Freeze()
+	defer func() {
+		fmt.Println("=== RESTORING TIME ===")
+		time.Unfreeze()
+	}()
 	code := m.Run()
 	os.Exit(code)
 }
